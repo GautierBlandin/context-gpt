90a8244789012eabbd2691265fa61379765db759
Gautier <gautier.blandin@gmail.com>
2024-07-28 17:40:38 +0200

    feat(chat): add chat store tests

---
+++ libs/chat/src/features/chat/chatDomain.store.test.ts
@@ -0,0 +1,65 @@
+import { LLMProxyFake } from '../../ports/LLMProxy.fake';
+import { LLMProxyDi } from '../../ports/LLMProxy.di';
+import { createChatDomainStoreFactory } from './chatDomain.store';
+
+describe('chat store', () => {
+  it('should send a user message', () => {
+    const { store } = setup();
+
+    store.getState().actions.sendMessage({ content: 'Hello', sender: 'User' });
+    expect(store.getState().messages).toEqual([{ content: 'Hello', sender: 'User' }]);
+  })
+
+  it('should handle the streaming response', async () => {
+    const { llmProxyFake, store } = setup();
+    vi.useFakeTimers();
+
+    llmProxyFake.setResponse(['Hello', ' World']);
+
+    store.getState().actions.sendMessage({ content: 'Hello', sender: 'User' });
+    expect(store.getState().streaming).toBe(true);
+
+    await vi.advanceTimersByTime(1);
+
+    expect(store.getState().messages).toEqual([
+      { content: 'Hello', sender: 'User' },
+      { content: 'Hello', sender: 'Assistant' },
+    ]);
+    expect(store.getState().streaming).toBe(true);
+
+    await vi.advanceTimersByTime(1);
+
+    expect(store.getState().messages).toEqual([
+      { content: 'Hello', sender: 'User' },
+      { content: 'Hello World', sender: 'Assistant' },
+    ]);
+    expect(store.getState().streaming).toBe(false);
+  });
+
+  it('should throw an error when sending a message while streaming', async () => {
+    const { store, llmProxyFake } = setup();
+
+    vi.useFakeTimers();
+
+    llmProxyFake.setResponse(['Hello', ' World']);
+
+    store.getState().actions.sendMessage({ content: 'Hello', sender: 'User' });
+
+    expect(store.getState().streaming).toBe(true);
+    expect(() => store.getState().actions.sendMessage({ content: 'Hello', sender: 'User' })).toThrowError('Cannot send message while streaming');
+  })
+})
+
+const setup = () => {
+  LLMProxyDi.reset();
+  const llmProxyFake = new LLMProxyFake();
+  llmProxyFake.setDelay(1);
+  LLMProxyDi.setOverride(llmProxyFake);
+
+  const store = createChatDomainStoreFactory();
+
+  return {
+    llmProxyFake,
+    store,
+  }
+}
