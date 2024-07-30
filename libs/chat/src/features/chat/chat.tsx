82610ab1e00af9b5bcc4bc384dbf54223dd949fe
Gautier <gautier.blandin@gmail.com>
2024-07-30 23:51:08 +0200

    feat(chat): add chat scrolling behaviour

--- libs/chat/src/features/chat/chat.tsx
+++ libs/chat/src/features/chat/chat.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { ChatDomainContext } from './chat-domain.context';
 import { createChatDomainStoreFactory } from './chat-domain.store';
-import React, { useContext, useMemo } from 'react';
+import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { WiredChatInput } from './chat-input';
 import { ChatMessages } from './chat-messages';
+import { useStore } from 'zustand';
 
 export function Chat() {
   const store = useMemo(() => createChatDomainStoreFactory(), []);
@@ -19,9 +20,32 @@ function ChatBody() {
   const store = useContext(ChatDomainContext);
   if (!store) throw new Error('ChatStore not provided in the component tree');
 
+  const messages = useStore(store, (state) => state.messages);
+
+  // To understand the scrolling logic, see the chat.scroll.mermaid file
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+  const [shouldScrollToBottom, setShouldScrollToBottom] = useState(true);
+
+  // Scroll to bottom when new messages are added
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current;
+    if (scrollContainer && shouldScrollToBottom) {
+      scrollContainer.scrollTop = scrollContainer.scrollHeight;
+    }
+  }, [messages, shouldScrollToBottom]);
+
+  const handleScroll = () => {
+    const scrollContainer = scrollContainerRef.current;
+    if (scrollContainer) {
+      const isScrolledToBottom =
+        Math.abs(scrollContainer.scrollHeight - scrollContainer.clientHeight - scrollContainer.scrollTop) < 1;
+      setShouldScrollToBottom(isScrolledToBottom);
+    }
+  };
+
   return (
     <div className="h-screen flex flex-col overflow-hidden" id={'full-chat'}>
-      <div className="flex-grow overflow-y-auto overflow-x-hidden">
+      <div ref={scrollContainerRef} className="flex-grow overflow-y-auto overflow-x-hidden" onScroll={handleScroll}>
         <div className="h-full w-full px-4">
           <div className="max-w-4xl mx-auto">
             <ChatMessages />
