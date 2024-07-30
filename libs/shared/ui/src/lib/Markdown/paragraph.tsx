df57998ec8de585f307df5e3c28d8814f31d7b69
Gautier <gautier.blandin@gmail.com>
2024-07-30 22:06:14 +0200

    feat(chat): remove padding from paragraphs

--- libs/shared/ui/src/lib/Markdown/paragraph.tsx
+++ libs/shared/ui/src/lib/Markdown/paragraph.tsx
@@ -1,7 +1,7 @@
 import { cn } from '../cn';
 
 export const Paragraph = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-  return <div className={cn(className, 'py-3')}>{children}</div>;
+  return <div className={cn(className)}>{children}</div>;
 };
 
 export const paragraph = {
