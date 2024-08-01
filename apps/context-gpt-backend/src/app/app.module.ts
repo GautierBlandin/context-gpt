b852b0645ff6d7142f081f583746521706eea7c3
Gautier <gautier.blandin@gmail.com>
2024-08-01 23:42:35 +0200

    feat(backend-api): add claude route

--- apps/context-gpt-backend/src/app/app.module.ts
+++ apps/context-gpt-backend/src/app/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClaudeController } from './claude.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [ClaudeController],
 })
 export class AppModule {}
