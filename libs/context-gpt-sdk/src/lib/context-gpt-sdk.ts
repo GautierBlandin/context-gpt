a2498cd108a65c5bfbadc8d0f10862ca30e5e78a
Gautier <gautier.blandin@gmail.com>
2024-08-04 23:36:30 +0200

    feat(sdk): add sdk for health and token

--- libs/context-gpt-sdk/src/lib/context-gpt-sdk.ts
+++ libs/context-gpt-sdk/src/lib/context-gpt-sdk.ts
@@ -1,3 +1,18 @@
-export function contextGptSdk(): string {
-  return 'context-gpt-sdk';
+import type { paths } from '../../api-types/schema';
+import createClient, { Client } from 'openapi-fetch';
+
+export class ContextGptSdk {
+  constructor(baseUrl: string) {
+    this.client = createClient<paths>({ baseUrl });
+  }
+
+  private client: Client<paths>;
+
+  public async checkToken({ token }: paths['/api/check-token']['post']['requestBody']['content']['application/json']) {
+    return this.client.POST('/api/check-token', { body: { token } });
+  }
+
+  public async healthCheck() {
+    return this.client.GET('/api/health');
+  }
 }
