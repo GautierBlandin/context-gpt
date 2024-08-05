cf4d8e3bf0ada96348f9031bab544cb11d118015
Gautier <gautier.blandin@gmail.com>
2024-08-05 23:32:23 +0200

    feat(sdk): improve claude controller

--- apps/context-gpt-backend/src/app/chat/claude.controller.ts
+++ apps/context-gpt-backend/src/app/chat/claude.controller.ts
@@ -3,6 +3,9 @@ import { Response } from 'express';
 import { Anthropic } from '@anthropic-ai/sdk';
 import { ClaudeRequestDto } from './claude.dto';
 
+/**
+ * This file is SSE voodoo. See https://medium.com/@david.richards.tech/sse-server-sent-events-using-a-post-request-without-eventsource-1c0bd6f14425 for more details.
+ */
 @Controller('claude')
 export class ClaudeController {
   private readonly anthropic: Anthropic;
@@ -17,8 +20,6 @@ export class ClaudeController {
 
   @Post()
   async handleClaudeRequest(@Body() claudeRequestDto: ClaudeRequestDto, @Res() res: Response) {
-    const { messages } = claudeRequestDto;
-
     // Set headers for SSE
     res.writeHead(200, {
       'Content-Type': 'text/event-stream',
@@ -27,15 +28,7 @@ export class ClaudeController {
     });
 
     try {
-      const anthropicStream = await this.anthropic.messages.create({
-        model: 'claude-3-5-sonnet-20240620',
-        max_tokens: 1000,
-        messages: messages.map((msg) => ({
-          role: msg.sender === 'User' ? 'user' : 'assistant',
-          content: msg.content,
-        })),
-        stream: true,
-      });
+      const anthropicStream = await this.initializeAnthropicStream(claudeRequestDto);
 
       for await (const chunk of anthropicStream) {
         if (chunk.type === 'content_block_delta' && 'text' in chunk.delta) {
@@ -49,5 +42,19 @@ export class ClaudeController {
     } finally {
       res.end();
     }
+  }
+
+  private async initializeAnthropicStream(claudeRequestDto: ClaudeRequestDto) {
+    const { messages } = claudeRequestDto;
+
+    return this.anthropic.messages.create({
+      model: 'claude-3-5-sonnet-20240620',
+      max_tokens: 1000,
+      messages: messages.map((msg) => ({
+        role: msg.sender === 'User' ? 'user' : 'assistant',
+        content: msg.content,
+      })),
+      stream: true,
+    });
   }
 }
