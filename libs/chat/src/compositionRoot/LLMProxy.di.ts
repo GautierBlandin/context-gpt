ff06e420c7609361fb73ad3fb72dfd972d8c4653
Gautier <gautier.blandin@gmail.com>
2024-08-09 00:22:51 +0200

    imports: shorten path

--- libs/chat/src/compositionRoot/LLMProxy.di.ts
+++ libs/chat/src/compositionRoot/LLMProxy.di.ts
@@ -1,6 +1,6 @@
 import { Claude3_5Proxy } from '../infrastructure';
 import { singletonFactory } from '@context-gpt/di';
-import { LLMProxy } from '../ports/LLMProxy';
+import { LLMProxy } from '../ports';
 
 export const LLMProxyDi = singletonFactory<LLMProxy>({
   factory: () => new Claude3_5Proxy(),
