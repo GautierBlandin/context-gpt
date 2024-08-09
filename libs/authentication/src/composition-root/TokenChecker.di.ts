399ee1882af0bd8a6608509732cf7ba83fdf9fa8
Gautier <gautier.blandin@gmail.com>
2024-08-09 23:56:06 +0200

    authentication: fix token checker di

--- libs/authentication/src/composition-root/TokenChecker.di.ts
+++ libs/authentication/src/composition-root/TokenChecker.di.ts
@@ -1,6 +1,7 @@
 import { singletonFactory } from '@context-gpt/di';
 import { TokenCheckerImpl } from '../infrastructure';
+import { TokenChecker } from '../ports';
 
-export const TokenCheckerSingleton = singletonFactory<TokenCheckerImpl>({
+export const TokenCheckerSingleton = singletonFactory<TokenChecker>({
   factory: () => new TokenCheckerImpl(),
 });
