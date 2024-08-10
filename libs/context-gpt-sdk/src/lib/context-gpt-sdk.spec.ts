24f1c5ce0cc1caed2ebb0a51ea387e1973dbbf7b
Gautier <gautier.blandin@gmail.com>
2024-08-10 22:58:24 +0200

    authentication: update sdk tests to incorporate authentication

--- libs/context-gpt-sdk/src/lib/context-gpt-sdk.spec.ts
+++ libs/context-gpt-sdk/src/lib/context-gpt-sdk.spec.ts
@@ -5,20 +5,22 @@ dotenv.config({ path: '.env.e2e' });
 
 const VALID_TOKEN = process.env['API_ACCESS_TOKEN'];
 
-describe('contextGptSdk', () => {
-  const sdk = new ContextGptSdk('http://localhost:8000');
+if (!VALID_TOKEN) {
+  throw new Error('API_ACCESS_TOKEN is not set in the environment variables');
+}
 
+describe('contextGptSdk', () => {
   describe('Check Token Endpoint (/check-token)', () => {
     it('should return true for a valid token', async () => {
-      if (!VALID_TOKEN) {
-        throw new Error('API_ACCESS_TOKEN is not set in the environment variables');
-      }
+      const { sdk } = setup();
 
       const result = await sdk.checkToken({ token: VALID_TOKEN });
       expect(result.data).toEqual({ isValid: true });
     });
 
     it('should return false for an invalid token', async () => {
+      const { sdk } = setup();
+
       const result = await sdk.checkToken({ token: 'invalid_token' });
       expect(result.data).toEqual({ isValid: false });
     });
@@ -26,6 +28,8 @@ describe('contextGptSdk', () => {
 
   describe('Health Endpoint (/health)', () => {
     it('should return OK status', async () => {
+      const { sdk } = setup();
+
       const result = await sdk.healthCheck();
       expect(result.data).toEqual({ status: 'OK' });
     });
@@ -33,6 +37,10 @@ describe('contextGptSdk', () => {
 
   describe('Claude Endpoint (/claude)', () => {
     it('should return the expected response when given valid input', async () => {
+      const { sdk } = setup();
+
+      sdk.setAccessToken(VALID_TOKEN);
+
       const messages = [
         {
           sender: 'User' as const,
@@ -65,5 +73,25 @@ describe('contextGptSdk', () => {
       expect(hasEnded).toBe(true);
       expect(result.trim()).toEqual('I understand, and my API is healthy!');
     });
+
+    it('should return 401 if no token is provided', async () => {
+      const { sdk } = setup();
+
+      for await (const chunk of sdk.promptClaude({ messages: [] })) {
+        expect(chunk).toMatchObject({
+          type: ChunkType.Error,
+          error: expect.stringContaining('401'),
+        });
+        break;
+      }
+    });
   });
 });
+
+const setup = () => {
+  const sdk = new ContextGptSdk('http://localhost:8000');
+
+  return {
+    sdk,
+  };
+};
