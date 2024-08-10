a9cc161deba1bec95ea86610268811e5bba23012
Gautier <gautier.blandin@gmail.com>
2024-08-10 23:45:11 +0200

    deploy: get API url from .env

--- libs/context-gpt-sdk/src/lib/singleton.ts
+++ libs/context-gpt-sdk/src/lib/singleton.ts
@@ -1,5 +1,11 @@
 import { ContextGptSdk } from './context-gpt-sdk';
 
+const API_URL = process?.env?.['NEXT_PUBLIC_API_URL'];
+
+if (!API_URL) {
+  throw new Error('NEXT_PUBLIC_API_URL is not set in the environment variables');
+}
+
 export function initializeSdkSingleton({ baseUrl }: Config) {
   const sdk = new ContextGptSdk(baseUrl);
 
@@ -12,4 +18,4 @@ export type Config = {
   baseUrl: string;
 };
 
-export const { getSdk } = initializeSdkSingleton({ baseUrl: 'http://localhost:8000' });
+export const { getSdk } = initializeSdkSingleton({ baseUrl: API_URL });
