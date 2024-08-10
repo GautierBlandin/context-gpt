ec51ab55dc485c26b8565b4e929c0418b2120a47
Gautier <gautier.blandin@gmail.com>
2024-08-10 17:51:23 +0200

    authentication: add WithAuth HOC

---
+++ libs/authentication/src/features/WithAuth.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSnapshot } from 'valtio';
+import { authenticationStore } from './Authentication.store';
+import { AuthenticationStateType } from '../core';
+import { Loader } from '@context-gpt/shared/ui';
+
+export function withAuth<PROPS extends React.JSX.IntrinsicAttributes>(WrappedComponent: React.ComponentType<PROPS>) {
+  return function AuthenticatedComponent(props: PROPS) {
+    const router = useRouter();
+    const auth = useSnapshot(authenticationStore);
+
+    useEffect(() => {
+      if (auth.authState.type === AuthenticationStateType.Anonymous) {
+        router.push('/login');
+      }
+    }, [auth.authState, router]);
+
+    if (auth.authState.type !== AuthenticationStateType.Authenticated) {
+      return <Loader size="large" />;
+    }
+
+    return <WrappedComponent {...props} />;
+  };
+}
