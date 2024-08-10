3e4acda8eb96be2f4f1b36296e1ae3a0c19cd700
Gautier <gautier.blandin@gmail.com>
2024-08-10 17:00:45 +0200

    code-style: loader stories

--- libs/shared/ui/src/lib/Loader.stories.tsx
+++ libs/shared/ui/src/lib/Loader.stories.tsx
@@ -1,8 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Loader } from './Loader';
-
-import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
 import { withContainer } from '../storybook-utils/container';
 
 const meta: Meta<typeof Loader> = {
@@ -17,37 +14,16 @@ export const Small: Story = {
   args: {
     size: 'small',
   },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const loader = canvas.getByRole('status');
-    expect(loader).toBeTruthy();
-    expect(loader.classList.contains('w-4')).toBeTruthy();
-    expect(loader.classList.contains('h-4')).toBeTruthy();
-  },
 };
 
 export const Medium: Story = {
   args: {
     size: 'medium',
   },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const loader = canvas.getByRole('status');
-    expect(loader).toBeTruthy();
-    expect(loader.classList.contains('w-8')).toBeTruthy();
-    expect(loader.classList.contains('h-8')).toBeTruthy();
-  },
 };
 
 export const Large: Story = {
   args: {
     size: 'large',
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const loader = canvas.getByRole('status');
-    expect(loader).toBeTruthy();
-    expect(loader.classList.contains('w-12')).toBeTruthy();
-    expect(loader.classList.contains('h-12')).toBeTruthy();
   },
 };
