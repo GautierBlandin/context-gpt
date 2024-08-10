8b234dec335dafd570f3d68a2459e21a5f3f9459
Gautier <gautier.blandin@gmail.com>
2024-08-10 17:51:43 +0200

    shared-ui: export loader and text input

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -2,3 +2,5 @@ export { TextArea } from './lib/TextArea';
 export { SubmitButton } from './lib/SubmitButton';
 export { ChatInput } from './lib/ChatInput';
 export { Markdown } from './lib/Markdown';
+export { TextInput } from './lib/TextInput';
+export { Loader } from './lib/Loader';
