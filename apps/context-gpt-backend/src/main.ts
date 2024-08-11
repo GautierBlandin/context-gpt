247c2dd7e6ea09be190b62872f45ebb62c2ffc59
Gautier <gautier.blandin@gmail.com>
2024-08-11 21:28:08 +0200

    deploy: run swagger only in dev mode

--- apps/context-gpt-backend/src/main.ts
+++ apps/context-gpt-backend/src/main.ts
@@ -1,17 +1,12 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
 import { AppModule } from './app/app.module';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 
 const projectRoot = path.join(__dirname, '../../../apps/context-gpt-backend');
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,24 +14,29 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
 
-  // Swagger configuration
-  const config = new DocumentBuilder()
-    .setTitle('Context-GPT API')
-    .setDescription('The Context-GPT API description')
-    .setVersion('1.0')
-    .addTag('context-gpt')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
+  if (isDevelopment) {
+    // Swagger configuration (only in development mode)
+    const config = new DocumentBuilder()
+      .setTitle('Context-GPT API')
+      .setDescription('The Context-GPT API description')
+      .setVersion('1.0')
+      .addTag('context-gpt')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
 
-  const swaggerSpecPath = path.join(projectRoot, 'swagger-spec.json');
-  fs.writeFileSync(swaggerSpecPath, JSON.stringify(document));
+    const swaggerSpecPath = path.join(projectRoot, 'swagger-spec.json');
+    fs.writeFileSync(swaggerSpecPath, JSON.stringify(document));
 
-  SwaggerModule.setup('api/docs', app, document);
+    SwaggerModule.setup('api/docs', app, document);
+  }
 
   const port = process.env.PORT || 8000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
-  Logger.log(`📚 Swagger documentation is available at: http://localhost:${port}/${globalPrefix}/docs`);
+
+  if (isDevelopment) {
+    Logger.log(`📚 Swagger documentation is available at: http://localhost:${port}/${globalPrefix}/docs`);
+  }
 }
 
 bootstrap();
