5852049e3ed06ac1153e69ddde312610800db7b8
Gautier <gautier.blandin@gmail.com>
2024-08-13 18:47:46 +0200

    deploy: add synced folder in infra script

--- apps/infrastructure/index.ts
+++ apps/infrastructure/index.ts
@@ -1,7 +1,14 @@
 import * as aws from '@pulumi/aws';
+import * as synced from '@pulumi/synced-folder';
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket('my-bucket-2');
+const bucket = new aws.s3.Bucket('next-static');
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+
+new synced.S3BucketFolder('synced-folder', {
+  path: '../../apps/context-gpt/out',
+  bucketName: bucket.bucket,
+  acl: 'private',
+});
