572576b3d74cf8b4144bb7216965a61659b2af51
Gautier <gautier.blandin@gmail.com>
2024-08-16 10:17:31 +0200

    project: move sdk tests to e2e project

--- apps/context-gpt-backend-e2e/src/api/api.spec.ts
+++ apps/context-gpt-backend-e2e/src/api/api.spec.ts
@@ -1,56 +1,94 @@
 import * as dotenv from 'dotenv';
-import { makeRequest } from '../support/makeRequest';
+import { ChunkType, ContextGptSdk } from '@context-gpt/context-gpt-sdk';
 
 dotenv.config({ path: '.env.e2e' });
 
 const VALID_TOKEN = process.env.API_ACCESS_TOKEN;
+const API_URL = process.env.API_URL || 'http://localhost:8000';
 
-describe('API End-to-End Tests', () => {
+describe('contextGptSdk', () => {
+  describe('Check Token Endpoint (/check-token)', () => {
+    it('should return true for a valid token', async () => {
+      const { sdk } = setup();
+
+      const result = await sdk.checkToken({ token: VALID_TOKEN });
+      expect(result.data).toEqual({ isValid: true });
+    });
+
+    it('should return false for an invalid token', async () => {
+      const { sdk } = setup();
+
+      const result = await sdk.checkToken({ token: 'invalid_token' });
+      expect(result.data).toEqual({ isValid: false });
+    });
+  });
+
   describe('Health Endpoint (/health)', () => {
     it('should return OK status', async () => {
-      const result = await makeRequest({ endpoint: '/health' });
-      expect(result).toEqual({ status: 'OK' });
+      const { sdk } = setup();
+
+      const result = await sdk.healthCheck();
+      expect(result.data).toEqual({ status: 'OK' });
     });
   });
 
   describe('Claude Endpoint (/claude)', () => {
     it('should return the expected response when given valid input', async () => {
+      const { sdk } = setup();
+
+      sdk.setAccessToken(VALID_TOKEN);
+
       const messages = [
         {
-          sender: 'User',
+          sender: 'User' as const,
           content:
             'The following message is a part of an end-to-end test to integrate you with our chatbot. If you understand this message, please response exactly with the following text: "I understand, and my API is healthy!"',
         },
       ];
-      const response = await makeRequest({
-        endpoint: '/claude',
-        method: 'POST',
-        data: { messages },
-        isSSE: true,
-      });
 
-      expect(response).toBeDefined();
-      expect(response).toEqual('I understand, and my API is healthy!');
+      let result = '';
+      let hasStarted = false;
+      let hasEnded = false;
+
+      for await (const chunk of sdk.promptClaude({ messages })) {
+        switch (chunk.type) {
+          case ChunkType.Start:
+            hasStarted = true;
+            break;
+          case ChunkType.Content:
+            result += chunk.content;
+            break;
+          case ChunkType.End:
+            hasEnded = true;
+            break;
+          case ChunkType.Error:
+            throw new Error(chunk.error);
+        }
+      }
+
+      expect(hasStarted).toBe(true);
+      expect(hasEnded).toBe(true);
+      expect(result.trim()).toEqual('I understand, and my API is healthy!');
     });
-  });
 
-  describe('Check Token Endpoint (/check-token)', () => {
-    it('should return true for a valid token', async () => {
-      const result = await makeRequest({
-        endpoint: '/check-token',
-        method: 'POST',
-        data: { token: VALID_TOKEN },
-      });
-      expect(result).toEqual({ isValid: true });
-    });
+    it('should return 401 if no token is provided', async () => {
+      const { sdk } = setup();
 
-    it('should return false for an invalid token', async () => {
-      const result = await makeRequest({
-        endpoint: '/check-token',
-        method: 'POST',
-        data: { token: 'invalid_token' },
+      const response = await sdk.promptClaude({ messages: [] }).next();
+      const chunk = response.value;
+
+      expect(chunk).toMatchObject({
+        type: ChunkType.Error,
+        error: expect.stringContaining('401'),
       });
-      expect(result).toEqual({ isValid: false });
     });
   });
 });
+
+const setup = () => {
+  const sdk = new ContextGptSdk(API_URL);
+
+  return {
+    sdk,
+  };
+};
