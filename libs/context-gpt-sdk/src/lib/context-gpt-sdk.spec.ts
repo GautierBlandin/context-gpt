973238d02e22752be1c077db59f92f5f200ba312
Gautier <gautier.blandin@gmail.com>
2024-08-16 10:18:54 +0200

    tests: add skipped tests

--- libs/context-gpt-sdk/src/lib/context-gpt-sdk.spec.ts
+++ libs/context-gpt-sdk/src/lib/context-gpt-sdk.spec.ts
@@ -2,3 +2,6 @@
  * Tests for the ContextGptSdk class are located in the context-gpt-backend-e2e project.
  * Instead of running separate API tests and SDK tests, we run them together are the SDK directly uses the API.
  */
+describe('ContextGptSdk', () => {
+  it.skip('shoul work');
+});
