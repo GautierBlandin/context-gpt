97477631cdebdf8b635c410202600638621926f5
Gautier <gautier.blandin@gmail.com>
2024-08-16 10:44:39 +0200

    tests: remove jest

--- apps/context-gpt-backend-e2e/src/support/test-setup.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import * as dotenv from 'dotenv';
-
-dotenv.config({ path: '.env.e2e' });
-
-module.exports = async function () {
-  // Configure axios for tests to use.
-  axios.defaults.baseURL = process.env.API_URL || 'http://localhost:8000';
-};
