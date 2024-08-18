1a9467b12bd171ce46d96d5400265ac00dce00c0
Gautier <gautier.blandin@gmail.com>
2024-08-18 21:41:45 +0200

    api: add swagger back to nest

--- apps/context-gpt-backend/src/main.ts
+++ apps/context-gpt-backend/src/main.ts
@@ -1,12 +1,24 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = process.env.API_PREFIX || '';
+
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description 2')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('openapi', app, document);
 
   const port = process.env.PORT || 8000;
   await app.listen(port);
