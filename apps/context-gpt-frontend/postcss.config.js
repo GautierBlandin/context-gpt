a2050ddaaf978a30e8c056cbd1e76b4ecee18ceb
Gautier <gautier.blandin@gmail.com>
2024-08-18 22:12:53 +0200

    package: change package.json to module

--- apps/context-gpt-frontend/postcss.config.js
+++ apps/context-gpt-frontend/postcss.config.js
@@ -1,11 +1,12 @@
-const { join } = require('path');
+import { join } from 'path';
 
+const __dirname = import.meta.dirname;
 // Note: If you use library-specific PostCSS/Tailwind configuration then you should remove the `postcssConfig` build
 // option from your application's configuration (i.e. project.json).
 //
 // See: https://nx.dev/guides/using-tailwind-css-in-react#step-4:-applying-configuration-to-libraries
 
-module.exports = {
+export default {
   plugins: {
     tailwindcss: {
       config: join(__dirname, 'tailwind.config.js'),
