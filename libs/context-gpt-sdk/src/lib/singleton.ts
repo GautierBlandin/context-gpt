33800d50274111d6935e0f3a7f2b6f1f92824bfb
Gautier <gautier.blandin@gmail.com>
2024-08-20 11:12:41 +0200

    sdk: revert proxying the sdk (bad fix)

--- libs/context-gpt-sdk/src/lib/singleton.ts
+++ libs/context-gpt-sdk/src/lib/singleton.ts
@@ -1,5 +1,4 @@
 import { ContextGptSdk } from './context-gpt-sdk';
-import { proxy } from 'valtio';
 
 const API_URL = import.meta.env['VITE_API_URL'] || '';
 const API_PREFIX = import.meta.env['VITE_API_PREFIX'] || '';
@@ -15,12 +14,8 @@ export function initializeSdkSingleton({ baseUrl, prefix }: Config) {
   const combinedBaseUrl = combineBaseUrl(baseUrl, prefix);
   const sdk = new ContextGptSdk({ baseUrl: combinedBaseUrl });
 
-  const state = proxy({
-    sdk,
-  });
-
   return {
-    getSdk: () => state.sdk,
+    getSdk: () => sdk,
   };
 }
 
