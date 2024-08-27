abf6dc0196f01868f607e3be6c0d91351de38376
Gautier <gautier.blandin@gmail.com>
2024-08-27 14:30:33 +0200

    auth: add shared module as a dependency of Auth module

--- apps/context-gpt-backend/src/app/auth/auth.module.ts
+++ apps/context-gpt-backend/src/app/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
+  imports: [SharedModule],
   controllers: [AuthController],
   providers: [AuthService, AuthGuard],
   exports: [AuthService, AuthGuard],
