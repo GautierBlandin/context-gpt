32fd40c4b2870cf4f32074ec1f240e2fba2ad22c
Gautier <gautier.blandin@gmail.com>
2024-08-27 19:26:37 +0200

    di: move di to libs/web/shared/di

--- libs/web/shared/di/vite.config.ts
+++ libs/web/shared/di/vite.config.ts
@@ -4,7 +4,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/libs/di',
+  cacheDir: '../../../../node_modules/.vite/libs/di',
 
   plugins: [nxViteTsPaths()],
 
@@ -19,6 +19,6 @@ export default defineConfig({
     environment: 'node',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
-    coverage: { reportsDirectory: '../../coverage/libs/di', provider: 'v8' },
+    coverage: { reportsDirectory: '../../../../coverage/libs/di', provider: 'v8' },
   },
 });
