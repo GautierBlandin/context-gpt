7bd4149edb23f263a5756a61ca75f611677bd006
Gautier <gautier.blandin@gmail.com>
2024-08-28 17:49:10 +0200

    tailwind-config: rename to cjs

--- apps/context-gpt-frontend/tailwind.config.cjs
+++ apps/context-gpt-frontend/tailwind.config.cjs
@@ -8,7 +8,15 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#606C38',
+        secondary: '#283618',
+        background: '#FEFAE0',
+        accent: '#DDA15E',
+        highlight: '#BC6C25',
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
