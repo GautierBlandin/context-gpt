7eaa7f5d5685f1381cfca5f162577a3d5dce936e
Gautier <gautier.blandin@gmail.com>
2024-08-30 20:26:02 +0200

    env: move to libs/server/shared/env

--- libs/server/shared/env/jest.config.ts
+++ libs/server/shared/env/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'server-shared',
-  preset: '../../../jest.preset.cjs',
+  preset: '../../../../jest.preset.cjs',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/server/shared',
+  coverageDirectory: '../../../../coverage/libs/server/shared/env',
 };
