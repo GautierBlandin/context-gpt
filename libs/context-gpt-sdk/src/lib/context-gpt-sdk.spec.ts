db42f117e1bca36def9db1daa74c1e7e35c7594d
Gautier <gautier.blandin@gmail.com>
2024-08-31 19:41:29 +0200

    sdk-rework: remove useless spec file

--- libs/context-gpt-sdk/src/lib/context-gpt-sdk.spec.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Tests for the ContextGptSdk class are located in the context-gpt-backend-e2e project.
- * Instead of running separate API tests and SDK tests, we run them together are the SDK directly uses the API.
- */
-describe('ContextGptSdk', () => {
-  it.skip('shoul work');
-});
