f24eeb707fecac0abe1897f6e72552ff2f17ccc3
Gautier <gautier.blandin@gmail.com>
2024-09-18 22:34:40 +0200

    feat: add error handling library

---
+++ libs/crossplatform/utils/errors/src/lib/result.ts
@@ -0,0 +1,25 @@
+export type Result<SUCCESS_TYPE, ERROR_TYPE> =
+  | {
+      type: 'success';
+      value: SUCCESS_TYPE;
+      error?: never;
+    }
+  | {
+      type: 'error';
+      error: ERROR_TYPE;
+      value?: never;
+    };
+
+export function success<SUCCESS_TYPE>(value: SUCCESS_TYPE): Result<SUCCESS_TYPE, never> {
+  return {
+    type: 'success',
+    value,
+  };
+}
+
+export function err<ERROR_TYPE>(error: ERROR_TYPE): Result<never, ERROR_TYPE> {
+  return {
+    type: 'error',
+    error,
+  };
+}
