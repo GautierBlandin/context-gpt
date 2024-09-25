d4586eea12db2c347388756bc65038936de20ae1
Gautier <gautier.blandin@gmail.com>
2024-09-25 23:07:58 +0200

    feat: remove defunct test-token file

--- libs/web/context-gpt-sdk/src/lib/test-token.ts
+++
@@ -1,9 +0,0 @@
-export function getValidToken() {
-  const VALID_TOKEN = import.meta.env['API_ACCESS_TOKEN'];
-
-  if (!VALID_TOKEN) {
-    throw new Error('API_ACCESS_TOKEN is not set in the environment variables');
-  }
-
-  return VALID_TOKEN;
-}
