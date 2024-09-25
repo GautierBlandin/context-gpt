4dd3dcc413387fae9dca32abca2198d39a3a5548
Gautier <gautier.blandin@gmail.com>
2024-09-25 22:44:40 +0200

    feat: import reflect-metadata in jest setup

--- libs/server/threads/jest.config.ts
+++ libs/server/threads/jest.config.ts
@@ -2,6 +2,7 @@
 export default {
   displayName: 'server-threads',
   preset: '../../../jest.preset.cjs',
+  setupFilesAfterEnv: ['./jest.setup.ts'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
