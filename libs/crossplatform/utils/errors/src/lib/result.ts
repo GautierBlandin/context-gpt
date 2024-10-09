e8f00381fd59016b6c2ef546579bccd18f355d2c
Gautier <gautier.blandin@gmail.com>
2024-10-09 09:26:15 +0200

    feat: improve error handling lib

--- libs/crossplatform/utils/errors/src/lib/result.ts
+++ libs/crossplatform/utils/errors/src/lib/result.ts
@@ -1,4 +1,4 @@
-export type Result<SUCCESS_TYPE, ERROR_TYPE> =
+export type Result<SUCCESS_TYPE, ERROR_TYPE extends Error> =
   | {
       type: 'success';
       value: SUCCESS_TYPE;
@@ -10,7 +10,7 @@ export type Result<SUCCESS_TYPE, ERROR_TYPE> =
       value?: never;
     };
 
-export type ResultAsync<SUCCESS_TYPE, ERROR_TYPE> = Promise<Result<SUCCESS_TYPE, ERROR_TYPE>>;
+export type ResultAsync<SUCCESS_TYPE, ERROR_TYPE extends Error> = Promise<Result<SUCCESS_TYPE, ERROR_TYPE>>;
 
 export function success<SUCCESS_TYPE>(value: SUCCESS_TYPE): Result<SUCCESS_TYPE, never> {
   return {
@@ -19,9 +19,32 @@ export function success<SUCCESS_TYPE>(value: SUCCESS_TYPE): Result<SUCCESS_TYPE,
   };
 }
 
-export function err<ERROR_TYPE>(error: ERROR_TYPE): Result<never, ERROR_TYPE> {
+export function err<ERROR_TYPE extends Error>(error: ERROR_TYPE): Result<never, ERROR_TYPE> {
   return {
     type: 'error',
     error,
   };
+}
+
+export function toThrowing<SUCCESS_TYPE, ERROR_TYPE extends Error>(
+  result: Result<SUCCESS_TYPE, ERROR_TYPE>,
+): SUCCESS_TYPE {
+  if (result.type === 'error') {
+    throw result.error;
+  }
+  return result.value;
+}
+
+export function toPromise<SUCCESS_TYPE, ERROR_TYPE extends Error>(
+  result: ResultAsync<SUCCESS_TYPE, ERROR_TYPE>,
+): Promise<SUCCESS_TYPE> {
+  return new Promise((resolve, reject) => {
+    result.then((result) => {
+      if (result.type === 'error') {
+        reject(result.error);
+      } else {
+        resolve(result.value);
+      }
+    });
+  });
 }
