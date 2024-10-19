fbfec50846db8c807c993f5c44606fd1127a7bff
Gautier <gautier.blandin@gmail.com>
2024-10-19 23:27:42 +0200

    feat: add zustand store experiment

---
+++ libs/web/chat/src/features/create-thread/create-thread.zustand-experiment.headless.ts
@@ -0,0 +1,32 @@
+import { Thread } from '../../core/Thread';
+import { MutationObserver, MutationObserverResult, QueryClient } from '@tanstack/query-core';
+import { StateCreator } from 'zustand';
+import { threadsRepositorySingleton } from '../../compositionRoot/threads.repository.singleton';
+import { toPromise } from '@context-gpt/errors';
+
+type CreateThreadState = Pick<MutationObserverResult<Thread, Error>, 'data' | 'error' | 'isPending'> & {
+  createThread: () => Promise<Thread>;
+};
+
+export const createThreadHeadlessStoreFactory: (queryClient: QueryClient) => StateCreator<CreateThreadState> =
+  (queryClient) => (set) => {
+    const threadsRepository = threadsRepositorySingleton.getInstance();
+    const mutationObserver = new MutationObserver(queryClient, {
+      mutationFn: () => toPromise(threadsRepository.createThread()),
+    });
+
+    mutationObserver.subscribe((mutationState) => {
+      set({
+        error: mutationState.error,
+        data: mutationState.data,
+        isPending: mutationState.isPending,
+      });
+    });
+
+    return {
+      error: mutationObserver.getCurrentResult().error,
+      data: mutationObserver.getCurrentResult().data,
+      isPending: mutationObserver.getCurrentResult().isPending,
+      createThread: () => mutationObserver.mutate(),
+    };
+  };
